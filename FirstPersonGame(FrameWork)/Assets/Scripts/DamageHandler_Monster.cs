5ad3610fd36a8428cae493fd80c3a4399c11a290
Austin West <korruptedanomaly@gmail.com>
2020-03-23 04:14:00 -0600

    Enemy follows and attacks

---
+++ FirstPersonGame(FrameWork)/Assets/Scripts/DamageHandler_Monster.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageHandler_Monster : vp_DamageHandler
+{
+    private Animator _animator;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        _animator = GetComponent<Animator>();
+    }
+    public override void Damage(vp_DamageInfo damageInfo)
+    {
+        base.Damage(damageInfo);
+
+        _animator.Play("Take Damage", 0, 0);
+    }
+}
